69d543bd328eba930ddae0cdddc7c75556316183
Patrick Nellessen <patrickqwertz@gmail.com>
2026-07-04 22:47:12 +0200

    add UserFlow, begin on simple lua behavior, add luacsharp package

---
+++ ArtifactsBackend/Behavior/Simple/Behaviors/SimpleLuaBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lua;
+
+namespace ArtifactsBackend.Behavior.Simple.Behaviors;
+
+public class SimpleLuaBehavior(string filePath, IDictionary<string, LuaValue> parameters) : SimpleBehavior
+{
+    /// <inheritdoc />
+    protected override async Task<Result> RunInternal()
+    {
+        LuaState state = LuaState.Create();
+        foreach ((string paramName, LuaValue paramValue) in parameters)
+        {
+            state.Environment[new LuaValue(paramName)] = paramValue;
+        }
+
+        // await state.DoFileAsync()
+        return Result.Success;
+    }
+}
